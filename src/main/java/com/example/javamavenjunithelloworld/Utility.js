37d47455ad7e4b6999b02ad03ba84385bcc405bf
raj-aceec <nagarajuboyapally9@gmail.com>
2024-09-14 15:16:33 +0530

    NagarajuB0001-Write the code to check if the given number is even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.js
@@ -0,0 +1,9 @@
+function nagaraju(){
+    console.log("I am Nagaraju Boyapally....!!!");
+}
+function EvenOrOdd(num){
+    if(num%2==0)
+        console.log(`${num} is a Even number`);
+    else
+        console.log(`${num} is a Odd number`);
+}
